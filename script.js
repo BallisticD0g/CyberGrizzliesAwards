aef1add61fe6fd2eec608084a37199a584c65149
Mikel <90412985+BallisticD0g@users.noreply.github.com>
2023-05-05 00:51:31 -0400

    Init

---
+++ script.js
@@ -0,0 +1,69 @@
+const texts = ["Akathen", "Social", "Carol"];
+const Award = ["Best VP", " Best GD", "BEST SA"]; // An array of texts
+let currentTextIndex = 0; // The current index in the texts array
+
+const textElement = document.getElementById("text");
+
+// Update the text element with the current text
+function updateText() {
+  textElement.textContent = texts[currentTextIndex];
+}
+
+// Move to the previous text in the array
+function moveToPrevText() {
+  currentTextIndex = (currentTextIndex - 1 + texts.length) % texts.length;
+  updateText();
+}
+
+// Move to the next text in the array
+function moveToNextText() {
+  currentTextIndex = (currentTextIndex + 1) % texts.length;
+  updateText();
+}
+
+// Add event listeners for the arrow keys
+document.addEventListener("keydown", function(event) {
+  if (event.code === "ArrowLeft") {
+    moveToPrevText();
+  } else if (event.code === "ArrowRight") {
+    moveToNextText();
+  }
+});
+
+// Initialize the text element with the first text
+updateText();
+
+
+ // An array of texts
+let AwardTextIndex = 0; // The current index in the texts array
+
+const AwardtextElement = document.getElementById("award");
+
+// Update the text element with the current text
+function updateText1() {
+    AwardtextElement.textContent = Award[AwardTextIndex];
+}
+
+// Move to the previous text in the array
+function moveToPrevText1() {
+    AwardTextIndex = (AwardTextIndex - 1 + Award.length) % Award.length;
+  updateText1();
+}
+
+// Move to the next text in the array
+function moveToNextText1() {
+    AwardTextIndex = (AwardTextIndex + 1) % Award.length;
+  updateText1();
+}
+
+// Add event listeners for the arrow keys
+document.addEventListener("keydown", function(event) {
+  if (event.code === "ArrowLeft") {
+    moveToPrevText1();
+  } else if (event.code === "ArrowRight") {
+    moveToNextText1();
+  }
+});
+
+// Initialize the text element with the first text
+updateText1();
